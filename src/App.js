bf0dc8be1388282c1909d1b9880cb665405ebd66
Jianing Zhang <jianing@acast.com>
2018-11-29 13:34:29 +0100

    hack: initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Typography from '@material-ui/core/Typography';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: "",
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    document.title = this.state.items;
+  }
+
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    document.title = this.state.items;
+  }
+
+  handleTextInputChange = (e) => {
+    this.setState({
+      item: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.item && this.state.item !== undefined && this.state.item !== '') {
+      this.setState({
+        items: [...this.state.items, this.state.item],
+        item: "",
+      });
+    }
+  };
+
+  handleDeleteClick = (index) => {
+    const newTodos = this.state.items.filter((_, todoIndex) => todoIndex !== index);
+    this.setState({
+      items: newTodos,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <Typography variant={"h1"} color={"primary"}>
+              <p>／人◕ __ ◕人＼</p>
+            </Typography>
+            <TodoForm
+                onSubmit={this.handleFormSubmit}
+                item={this.state.item}
+                onChange={this.handleTextInputChange}/>
+            <TodoList
+                onClick={this.handleDeleteClick}
+                items={this.state.items}/>
+          </header>
+        </div>
     );
   }
 }
